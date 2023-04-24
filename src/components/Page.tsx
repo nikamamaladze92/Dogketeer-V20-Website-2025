8626b4613d169ffef62837c68b6f8fa92edf0ade
jaenixlee <jaeni_lee@yahoo.com>
2023-04-24 17:01:38 -0400

    get started

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import Navbar from './Navbar';
 import Home from './Home';
 import Features from './Features';
-import Demo from './Demo';
-import GetStarted from './GetStarted';
+import Demo from './Demo/Demo';
+import GetStarted from './GetStarted/GetStarted';
 import Team from './Team';
 
 const Page = (): JSX.Element => {
