851bec0525f0dade97dea9f70d60d9fe621a813e
jaysalgado <jaysalgado97@gmail.com>
2023-04-28 17:05:01 -0400

    get new

--- src/components/Features/Features.tsx
+++ src/components/Features/Features.tsx
@@ -11,10 +11,9 @@ import security from '../../assets/features/security.svg';
 import prune from '../../assets/features/prune.svg';
 
 function Features() {
-<<<<<<< HEAD
   const [isLoaded, setIsLoaded] = useState(false);
   const [isIntersecting, setIsIntersecting] = useState(false);
-  const demoRef = useRef<HTMLDivElement>(null);
+  const featureRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const observer = new IntersectionObserver(
@@ -27,10 +26,10 @@ function Features() {
           setIsIntersecting(false);
         }
       },
-      { threshold: 0.5 }
+      { threshold: 0.4 }
     );
 
-    const currentRef = demoRef.current;
+    const currentRef = featureRef.current;
 
     if (currentRef) {
       observer.observe(currentRef);
@@ -42,16 +41,14 @@ function Features() {
       }
     };
   }, []);
-=======
-
->>>>>>> 03201b5ea5b2943c81e8d4131026f09eab5b0785
-
   return (
-    <div className={styles.featuresContainer}>
+    <div
+      className={isLoaded ? styles.featuresContainer : styles.featuresNo}
+      ref={featureRef}
+    >
       <div className={styles.features}>
         <h1>Features</h1>
-<<<<<<< HEAD
-        <a className={styles.article} href="http://google.com">
+        <a className={styles.article} href="http://google.com" target="_blank">
           Read about us on Medium!
         </a>
         <div className={styles.featureCards}>
@@ -81,6 +78,7 @@ function Features() {
               'Access metrics related to Kubernetes clusters with just the toggle of a button'
             }
           />
+
           <FeatureCard
             header={'Detailed Process Logs'}
             img={logs}
@@ -105,21 +103,6 @@ function Features() {
             img={prune}
             info={'Clear up unused images to ensure optimized performance'}
           />
-=======
-        <a className={styles.article} href="http://google.com" target="_blank">Read about us on Medium!</a>
-        <div className={styles.featureCards}>
-        
-          <FeatureCard header={"Container Management"} img={containers} info={"Create, start, stop, and delete Docker containers all in one place"} />
-          <FeatureCard header={"Image Management"} img={images} info={"Easily view, run, remove, and search for Docker images"} />
-          <FeatureCard header={"Container Metrics"} img={metrics} info={"Access metrics related to containers in a dashboard with rich visualizations"} />
-          <FeatureCard header={"Cluster Metrics"} img={kubernetes} info={"Access metrics related to Kubernetes clusters with just the toggle of a button"} />
-          
-          <FeatureCard header={"Detailed Process Logs"} img={logs} info={"View and download process logs related to containers"} />
-          <FeatureCard header={"Secure Authentication"} img={security} info={"Maintain trust in Docketeer's encryption and hashing methods"} />
-          <FeatureCard header={"Sessions"} img={cookie} info={"Stay logged in thanks to Docketeer's implementation of sessions and cookies"} />
-          <FeatureCard header={"System Prune"} img={prune} info={"Clear up unused images to ensure optimized performance"} />
-        
->>>>>>> 03201b5ea5b2943c81e8d4131026f09eab5b0785
         </div>
       </div>
     </div>
