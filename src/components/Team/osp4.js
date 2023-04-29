e2ebbeaedd04daba27521ddee6a0fd0d815802ac
jaysalgado <jaysalgado97@gmail.com>
2023-04-29 14:04:33 -0400

    pic

--- src/components/Team/osp4.js
+++ src/components/Team/osp4.js
@@ -1,32 +1,33 @@
-import anna from "../../assets/anna.png";
-import jaeni from "../../assets/jaeni.jpeg"
-import emily from "../../assets/emily.png"
-//import jay from "../../assets/jay.png"
-const osp4 = [{
-    "img": anna,
-    "name": "Anna Tran",
-    "linkedin": "https://www.linkedin.com/in/annatran10/",
-    "github": "https://github.com/annamullike"
-},
-{
-    "img": emily,
-    "name": "Emily Johl",
-    "linkedin": "https://www.linkedin.com/in/emily-johl-5093ab137/",
-    "github": "https://github.com/emilyjohl"
-}
-,
-{
-    "img": jaeni,
-    "name": "Jaeni Lee",
-    "linkedin": "https://www.linkedin.com/in/jaenilee/",
-    "github": "https://github.com/jaenixlee"
-}
-,
-{
-    "img":"https://images.unsplash.com/photo-1618359057154-e21ae64350b6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
-    "name": "Jay Salgado",
-    "linkedin": "https://www.linkedin.com/in/joseph-salgado-76410620b/",
-    "github": "https://github.com/Jaysalgado"
-}]
+import anna from '../../assets/anna.png';
+import jaeni from '../../assets/jaeni.jpeg';
+import emily from '../../assets/emily.png';
+import jay from '../../assets/jay.jpg';
+
+const osp4 = [
+  {
+    img: anna,
+    name: 'Anna Tran',
+    linkedin: 'https://www.linkedin.com/in/annatran10/',
+    github: 'https://github.com/annamullike',
+  },
+  {
+    img: emily,
+    name: 'Emily Johl',
+    linkedin: 'https://www.linkedin.com/in/emily-johl-5093ab137/',
+    github: 'https://github.com/emilyjohl',
+  },
+  {
+    img: jaeni,
+    name: 'Jaeni Lee',
+    linkedin: 'https://www.linkedin.com/in/jaenilee/',
+    github: 'https://github.com/jaenixlee',
+  },
+  {
+    img: jay,
+    name: 'Jay Salgado',
+    linkedin: 'https://www.linkedin.com/in/joseph-salgado-76410620b/',
+    github: 'https://github.com/Jaysalgado',
+  },
+];
 
 export default osp4;
