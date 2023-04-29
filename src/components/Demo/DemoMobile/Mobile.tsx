59dc2cc86be684d8b126bab2995ab98487e553c6
jaysalgado <jaysalgado97@gmail.com>
2023-04-29 10:06:51 -0400

    mobile demo

--- src/components/Demo/DemoMobile/Mobile.tsx
+++ src/components/Demo/DemoMobile/Mobile.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import styles from './Demo.module.scss';
 import containers from '../../../assets/containers-demo.gif';
 import images from '../../../assets/images-demo.gif';
@@ -8,11 +8,50 @@ import logs from '../../../assets/logs-demo.gif';
 import styles from '../Demo.module.scss';
 
 const Mobile = (): JSX.Element => {
-  const gifs = [containers, images, containerMetrics, k8sMetrics, logs];
+  const gifs = [
+    { name: 'containers', url: containers },
+    { name: 'images', url: images },
+    { name: 'containerMetrics', url: containerMetrics },
+    { name: 'k8sMetrics', url: k8sMetrics },
+    { name: 'logs', url: logs },
+  ];
+  const [currGif, setGif] = useState(0);
+  const handleClick = (direction: string) => {
+    if (direction === 'right') {
+      if (currGif === 4) {
+        return setGif(0);
+      } else {
+        const change = currGif + 1;
+        return setGif(change);
+      }
+    } else {
+      if (currGif === 0) {
+        return setGif(4);
+      } else {
+        const change = currGif - 1;
+        return setGif(change);
+      }
+    }
+  };
 
   return (
     <div>
-      <img className={styles.gifMobile} src={gifs[0]}></img>
+      <img className={styles.gifMobile} src={gifs[currGif].url}></img>
+      <div>{gifs[currGif].name}</div>
+      <div className={styles.gifMobileButtonsBox}>
+        <div
+          className={styles.gifMobileButtons}
+          onClick={() => handleClick('')}
+        >
+          Go left
+        </div>
+        <div
+          className={styles.gifMobileButtons}
+          onClick={() => handleClick('right')}
+        >
+          Go right
+        </div>
+      </div>
     </div>
   );
 };
