dfd69efd5ee452e3ea3f9eb16436c11571fe9f73
jaysalgado <jaysalgado97@gmail.com>
2023-05-03 16:48:22 -0400

    testing complete

--- __mocks__/fileMock.js
+++ __mocks__/fileMock.js
@@ -1,15 +1,1 @@
-// const containersGif = 'containersGif';
-// const imagesGif = 'imagesGif';
-// const containerMetricsGif = 'containerMetricsGif';
-// const clusterMetricsGif = 'clusterMetricsGif';
-// const logsGif = 'logsGif';
-
-// export default {
-//   '../src/assets/containers-demo.gif': containersGif,
-//   '../src/assets/images-demo.gif': imagesGif,
-//   '../src/assets/containermetrics-demo.gif': containerMetricsGif,
-//   '../src/assets/k8smetrics-demo.gif': clusterMetricsGif,
-//   '../src/assets/logs-demo.gif': logsGif,
-// };
-
 module.exports = '';
