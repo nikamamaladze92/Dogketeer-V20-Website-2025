afb9021883280c7f3669ed0d90df9415d3160ad7
Cristian Morales <crismorales@protonmail.com>
2023-10-28 13:03:21 -0500

    Fixed the Get Started section by adding a dockerhub link and letting users know they can just go to the extension marketplace or dockerhub and install from there. Moved the buttons to a Button Container Div, and added some margins and styling to them.

--- src/components/GetStarted/GetStarted.tsx
+++ src/components/GetStarted/GetStarted.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './GetStarted.module.scss';
 import githubLogo from '../../assets/github-mark/github-mark-white.png';
+import dockerhubLogo from '../../assets/dockerhub-mark/04-symbol_light-blue-docker-logo.png';
 
 const GetStarted = (): JSX.Element => {
   return (
@@ -10,15 +11,28 @@ const GetStarted = (): JSX.Element => {
         Getting started with Docketeer is easy!
         <br />
         Navigate to our GitHub page and follow our detailed instructions under
-        "Installation"
+        "Installation".
+        <br />
+        Or find us on the Docker Desktop Extensions Marketplace and simply click
+        install.
       </p>
-      <div className={styles.githubButton}>
-        <a
-          className={styles.githubLink}
-          href='https://github.com/open-source-labs/Docketeer-Extension'
-          target='blank'>
-          GitHub <img className={styles.logo} src={githubLogo}></img>
-        </a>
+      <div className={styles.buttonContainer}>
+        <div className={styles.githubButton}>
+          <a
+            className={styles.githubLink}
+            href='https://github.com/open-source-labs/Docketeer-Extension'
+            target='blank'>
+            GitHub <img className={styles.logo} src={githubLogo}></img>
+          </a>
+        </div>
+        <div className={styles.githubButton}>
+          <a
+            className={styles.githubLink}
+            href='https://hub.docker.com/extensions/docketeerxiv/docketeer-extension'
+            target='blank'>
+            Dockerhub <img className={styles.logo} src={dockerhubLogo}></img>
+          </a>
+        </div>
       </div>
     </div>
   );
